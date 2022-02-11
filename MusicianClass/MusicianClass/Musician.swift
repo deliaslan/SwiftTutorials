8543fb5ed19370eb307c310881fd8b22ae203e67
Adem Deliaslan <deliaslan@gmail.com>
2022-02-11 15:39:28 +0300

    Swift - OOP initialize - constructor kullanımına giriş.
    Kendi oluşturduğumuz bir sınıftan nasıl nesne türetirizi anlatıyor.

---
+++ MusicianClass/MusicianClass/Musician.swift
@@ -0,0 +1,20 @@
+//
+//  Musician.swift
+//  MusicianClass
+//
+//  Created by Adem Deliaslan on 11.02.2022.
+//
+
+import Foundation
+
+class Musician {
+    var name: String = ""
+    var age: Int = 0
+    var instrument: String = ""
+    
+    
+    //Initializer - diğer dillerde Constructor yapısına karşılık gelir.
+    init() {
+        print("Musician created")
+    }
+}
---
+++ MusicianClass/MusicianClass/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  MusicianClass
+//
+//  Created by Adem Deliaslan on 11.02.2022.
+//
+
+import Foundation
+
+
+let james = Musician()
+let adem = Musician()
+
+james.name = "James Hetfield"
+james.age = 38
+james.instrument = "Guitar"
+
+
+print(james.age)
+print(james.instrument)
