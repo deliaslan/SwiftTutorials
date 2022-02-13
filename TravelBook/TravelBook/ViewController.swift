2b2f28a397e239e1e216bfcc2d357cb33340da9e
Adem Deliaslan <deliaslan@gmail.com>
2022-02-13 20:31:33 +0300

    Swift - Harita kısmı üzerinde pinch olayı yapıldı. Zoom olayı gerçekleştirildi. Simülatörde custom coordinate düzenlendi.

---
+++ TravelBook/TravelBook/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  TravelBook
+//
+//  Created by Adem Deliaslan on 13.02.2022.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        //harita bilgisini ekrana yazma ve konum gösterme
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+        //gesturRecognizer
+        let gesturRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer: )))
+        gesturRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gesturRecognizer)
+    }
+    
+    //gesture recognizer Metod
+    @objc func chooseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let touchPoint = gestureRecognizer.location(in: self.mapView)
+            let touchedCoordinates = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+            
+            let annotaion = MKPointAnnotation()
+            annotaion.coordinate = touchedCoordinates
+            annotaion.title = "New Annotaition"
+            annotaion.subtitle = "Tarvel Book"
+            self.mapView.addAnnotation(annotaion)
+        }
+    }
+    
+    //locationManager metodu
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+
+
+}
+
