7a991b9ab3e6e66643176357b0bb2e1d539a3df6
Adem Deliaslan <deliaslan@gmail.com>
2022-02-14 23:28:06 +0300

    Swift - TravelBook kaydedilen veri listede gösterilmektedir. Ama canlı olarak gelmiyor henüz.
    Uygulamayı kapatıp açınca ya da yeniden çalıştırınca veri görünmektedir. Bir sonraki geliştirmede bu durumu düzenleyeceğiz.

--- TravelBook/TravelBook/ListViewController.swift
+++ TravelBook/TravelBook/ListViewController.swift
@@ -6,24 +6,80 @@
 //
 
 import UIKit
+import CoreData
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
 
+    var titleArray = [String]()
+    var idArray = [UUID]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        //top bar a buton ekleme
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getData()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func addButtonClicked() {
+        performSegue(withIdentifier: "toViewController", sender: nil)
     }
-    */
+    
+    
+    
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                
+                //listeye aynı değerleri her kaydedişte tekrar yazmasın diye.
+                //(veritabanında sadece bir kayıt varken liste her kaydedişte sayıyı katlamaktadır yoksa.)
+                self.titleArray.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+                
+                //verinin liste olarak gösterilmesi
+                for result in results as! [NSManagedObject]{
+                    if let title = result.value(forKey: "title") as? String {
+                        self.titleArray.append(title)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    
+                    tableView.reloadData()
+                }
+            }
+        }
+        catch {
+            print("Data fetch error")
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = titleArray[indexPath.row]
+        return cell
+    }
+    
 
 }
