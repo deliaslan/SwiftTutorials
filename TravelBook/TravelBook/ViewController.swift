fc7c8136e48d9eb7b4da4083cc388b05487dba97
Adem Deliaslan <deliaslan@gmail.com>
2022-02-14 23:00:52 +0300

    Swift - Travle Book MapKit Kullanımı - Haritada bir noktayı veritabanına kaydetme işlemi  gerçekleştirildi.

--- TravelBook/TravelBook/ViewController.swift
+++ TravelBook/TravelBook/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CoreData
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var commentText: UITextField!
+    
     var locationManager = CLLocationManager()
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
     
     
     override func viewDidLoad() {
@@ -41,10 +47,13 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             let touchPoint = gestureRecognizer.location(in: self.mapView)
             let touchedCoordinates = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
             
+            chosenLatitude = touchedCoordinates.latitude
+            chosenLongitude = touchedCoordinates.longitude
+            
             let annotaion = MKPointAnnotation()
             annotaion.coordinate = touchedCoordinates
-            annotaion.title = "New Annotaition"
-            annotaion.subtitle = "Tarvel Book"
+            annotaion.title = nameText.text
+            annotaion.subtitle = commentText.text
             self.mapView.addAnnotation(annotaion)
         }
     }
@@ -57,6 +66,25 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         mapView.setRegion(region, animated: true)
     }
 
-
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        newPlace.setValue(nameText.text, forKey: "title")
+        newPlace.setValue(commentText.text, forKey: "subtitle")
+        newPlace.setValue(chosenLatitude, forKey: "latitude")
+        newPlace.setValue(chosenLongitude, forKey: "longitude")
+        newPlace.setValue(UUID(), forKey: "id")
+        
+        do{
+            try context.save()
+            print("Success")
+        } catch {
+            print("Error")
+        }
+        
+    }
+    
 }
 
