d0ab9f154d3b398f53758f4ca633e0006a624b72
Adem Deliaslan <deliaslan@gmail.com>
2022-02-17 13:29:21 +0300

    Swift - Instagram Clone Firebase yüklendi, Login, SignUp işlemi gerçekleşmekte.
    Oluşturulan Insta Clone App içerisinde tab controller ve segue configuration yapıldı.
    * Tabbar Controller üzerinden Storyboard segue kısmında
    Kind: Present Modally
    Presentation: Full Screen olarak düzenlendi.
    
    Alert fonksiyonu yazıldı.
    Firebase üzerinden gelen hataların bu şekilde gösterilmesi sağlandı.
    
    Not: M1 işlemcili Mac üzerinde iken CocoaPods un düzgün bir şkeilde gereksinimleri yüklemesi için: Proje klasörü içerisinde iken arch -x86_64 pod install dememiz gerekiyor. Bu yeterli olmazsa:  sudo arch -x86_64 gem install ffi komutunu çalıştırıp sonra tekrar deneyiniz. Uzun sürüyor ama işlem tamamlanıyor.(Yükleme süreci)

---
+++ InstagramCloneWithFireBase/InstagramCloneWithFireBase/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  InstagramCloneWithFireBase
+//
+//  Created by Adem Deliaslan on 17.02.2022.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInClicked(_ sender: Any) {
+      //  performSegue(withIdentifier: "toFeedVC", sender: nil)  //test code
+        if passwordText.text != "" && emailText.text != "" {
+            
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                if error != nil {
+                    self.makeAlert(title: "Error!", message: error?.localizedDescription ?? "Error", btnTitle: "OK")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        } else {
+            makeAlert(title: "Error!", message: "Username and Password required", btnTitle: "OK")
+        }
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        
+        if passwordText.text != "" && emailText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text! ) { authdata, error in
+                if error != nil {
+                    self.makeAlert(title: "Error!", message: error?.localizedDescription ?? "Error", btnTitle: "OK")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        } else {
+            
+            makeAlert(title: "Error!", message: "Username and Password required", btnTitle: "OK")
+            
+        }
+       
+        
+    }
+    
+    
+    func makeAlert(title: String?, message: String?, btnTitle: String?){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let ok = UIAlertAction(title: btnTitle, style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
