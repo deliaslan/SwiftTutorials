7c0abb2227d222877f4c83a27808343075927d88
Adem Deliaslan <deliaslan@gmail.com>
2022-02-20 10:27:54 +0300

    Swift- Prototip cell FeedViewController da tanımlanarak Feed page test edildi.
    Buradaki süreçte prototip cell identifier ı ve kendi oluşturduğumuz cell i nasıl kullanacağımız ön plana çıkmaktadır.
    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedCell kod parçası ile identifier i cell olan ve FeedCell olarak oluşturduğumuz cell imizi dequeueReusableCell ile eklediğimizi öğrendik.

---
+++ InstagramCloneWithFireBase/InstagramCloneWithFireBase/FeedCell.swift
@@ -0,0 +1,29 @@
+//
+//  FeedCell.swift
+//  InstagramCloneWithFireBase
+//
+//  Created by Adem Deliaslan on 20.02.2022.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var userImageVİew: UIImageView!
+    @IBOutlet weak var userCommentLabel: UILabel!
+    @IBOutlet weak var userLikeCounterLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func userLikeButton(_ sender: Any) {
+    }
+}
--- InstagramCloneWithFireBase/InstagramCloneWithFireBase/FeedViewController.swift
+++ InstagramCloneWithFireBase/InstagramCloneWithFireBase/FeedViewController.swift
@@ -7,24 +7,30 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
 
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedCell
+        cell.userEmailLabel.text = "Test@test.com"
+        cell.userCommentLabel.text = "Comment Area"
+        cell.userLikeCounterLabel.text = String(20)
+        cell.userImageVİew.image = UIImage(named: "select_image")
+        return cell
+    }
 }
