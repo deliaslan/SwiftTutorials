0ad76dc7805dc09d02414cddde31c31ca2cf9f82
Adem Deliaslan <deliaslan@gmail.com>
2022-02-22 12:49:17 +0300

    Parse where komutu ile veritabanından verileri filtre ederek çağırma işlemi gerçekleştirildi.

--- FoursquareClone/FoursquareClone/ViewController.swift
+++ FoursquareClone/FoursquareClone/ViewController.swift
@@ -9,27 +9,28 @@ import UIKit
 import Parse //eklendi
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-//        let parseObject = PFObject(className: "Okul")
-//        parseObject["name"] = "Hoca Ahmet Yesevi Üniversitesi"
-//        parseObject["department"] = "Computer Engineering"
-//        parseObject["class"] = 4
-//
-//        parseObject.saveInBackground { success, error in
-//            if error != nil {
-//                print(error?.localizedDescription)
-//            } else {
-//                print("Success uploaded")
-//            }
-//
-//        }
+        //        let parseObject = PFObject(className: "Okul")
+        //        parseObject["name"] = "Hoca Ahmet Yesevi Üniversitesi"
+        //        parseObject["department"] = "Computer Engineering"
+        //        parseObject["class"] = 4
+        //
+        //        parseObject.saveInBackground { success, error in
+        //            if error != nil {
+        //                print(error?.localizedDescription)
+        //            } else {
+        //                print("Success uploaded")
+        //            }
+        //
+        //        }
         
         
         let query = PFQuery(className: "Okul")
+        query.whereKey("name", equalTo: "Sakarya Üniversitesi")
         query.findObjectsInBackground { objects, error in
             if error != nil {
                 print(error?.localizedDescription)
@@ -39,7 +40,7 @@ class ViewController: UIViewController {
         }
         
     }
-
-
+    
+    
 }
 
