d0de2d584f790d09ee78a0da82395dadbdbdd660
Adem Deliaslan <deliaslan@gmail.com>
2022-02-22 12:23:14 +0300

    Swift - Foursquare Clone :
     pod içerisinden parse yüklendi.

---
+++ FoursquareClone/FoursquareClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Adem Deliaslan on 22.02.2022.
+//
+
+import UIKit
+import Parse //eklendi
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
