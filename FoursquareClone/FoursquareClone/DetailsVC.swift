ad98bcbcc20886138e75f12716f485a2f04cd5ae
Adem Deliaslan <deliaslan@gmail.com>
2022-02-26 01:13:32 +0300

    Harita üzerindeki pine tıklanınca bilgi vermesi ve info ile de haritayı açarak yol tarifi alma işlemi gerçekleştirildi.

--- FoursquareClone/FoursquareClone/DetailsVC.swift
+++ FoursquareClone/FoursquareClone/DetailsVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit //eklendi
 import Parse
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController,MKMapViewDelegate {
     @IBOutlet weak var detailsPlaceName: UILabel!
     @IBOutlet weak var detailsPlaceType: UILabel!
     @IBOutlet weak var detailsDescription: UILabel!
@@ -25,6 +25,8 @@ class DetailsVC: UIViewController {
         
         // Do any additional setup after loading the view.
         getDataFromParse()
+        
+        detailsMapView.delegate = self
         
     }
     
@@ -75,6 +77,9 @@ class DetailsVC: UIViewController {
                             }
                         }
                     }
+                    
+                    
+                    
                     //MAPS
                     let location = CLLocationCoordinate2D(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
                     let span = MKCoordinateSpan(latitudeDelta: 0.003, longitudeDelta: 0.003)
@@ -82,12 +87,51 @@ class DetailsVC: UIViewController {
                     
                     self.detailsMapView.setRegion(region, animated: true)
                     
-                    //harita üzerine annotation ekleme 
+                    //harita üzerine annotation ekleme
                     let annotation = MKPointAnnotation()
                     annotation.coordinate = location
                     annotation.title = self.detailsPlaceName.text!
                     annotation.subtitle = self.detailsPlaceType.text!
                     self.detailsMapView.addAnnotation(annotation)
+                    
+                }
+            }
+        }
+    }
+    
+    //map info part
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let reUseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reUseId)
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reUseId)
+            pinView?.canShowCallout = true
+            let button = UIButton(type: UIButton.ButtonType.detailDisclosure)
+            pinView?.rightCalloutAccessoryView = button
+        } else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if self.chosenLongitude != 0.0 && self.chosenLatitude != 0.0 {
+            let requestLocation = CLLocation(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { placeMarks, error in
+                if let placeMark = placeMarks {
+                    if placeMark.count > 0 {
+                        let mkPlaceMark = MKPlacemark(placemark: placeMark[0])
+                        let mapItem = MKMapItem(placemark: mkPlaceMark)
+                        mapItem.name = self.detailsPlaceName.text!
+                        
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+                        mapItem.openInMaps(launchOptions: launchOptions)
+                    }
                 }
             }
         }
