a89067d94638b82c43a05d6413320029b6281dcd
Adem Deliaslan <deliaslan@gmail.com>
2022-02-27 10:24:52 +0300

    ML handle kısmını oluşturduk ve gösterilen görselin tahmini% ile ne olduğunu ekrana yazdık.
    Handler kısmınıda yaptığımız bu işlemde DispatchQueue.global(qos: .userInteractive).async kullanma sebebimiz arkaplanda hızlı bir şekilde işlenmesini önceliklendrimek içindir.

--- MachineLearningImageRecognition/MachineLearningImageRecognition/ViewController.swift
+++ MachineLearningImageRecognition/MachineLearningImageRecognition/ViewController.swift
@@ -20,16 +20,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func changeButtonClicked(_ sender: Any) {
         
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .photoLibrary
         present(picker, animated: true, completion: nil)
-        
     }
-    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
@@ -38,15 +36,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let ciImage = CIImage(image: imageView.image!) {
             chosenImage = ciImage
         }
-        
         recognizeImage(image: chosenImage)
     }
     
-    
     func recognizeImage(image: CIImage) {
-        
         //1 request
         //2 handler
+        resultLabel.text = "Finding..."
         if let model = try? VNCoreMLModel(for: MobileNetV2().model) {
             let request = VNCoreMLRequest(model: model) { vnrequest, error in
                 if let results = vnrequest.results as? [VNClassificationObservation] {
@@ -54,9 +50,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                         let topResult = results.first
                         DispatchQueue.main.async {
                             let confidenceLevel = (topResult?.confidence ?? 0) * 100 //verilen değer 0-1 arasında olduğundan bu değeri % olarak göstermek için 100 ile çarpıyoruz.
-                            self.resultLabel.text = "\(confidenceLevel)% : It's a/an \(topResult?.identifier)"
+                            let rounded = String(format: "%.2f",confidenceLevel)
+                            self.resultLabel.text = "\(rounded)% : It's \(topResult!.identifier)"
                         }
                     }
+                }
+            }
+            let handler = VNImageRequestHandler(ciImage: image)
+            DispatchQueue.global(qos: .userInteractive).async { //global arka plandaki işlerin önceliklendirilmesini en üst seviyeye çıkarır.
+                do{
+                    try handler.perform([request])
+                } catch {
+                    print("Error :\(error.localizedDescription)")
                 }
             }
         }
