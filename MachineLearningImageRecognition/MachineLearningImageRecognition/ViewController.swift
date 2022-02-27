32e51839b72bc1c04764a844e2e472183497b58a
Adem Deliaslan <deliaslan@gmail.com>
2022-02-27 09:23:48 +0300

    Swift Machine Learning App Uygulaması
    Uygulamanın arayüzü oluşturuldu(imageView, label, button)
    https://developer.apple.com/machine-learning/models/ adresinden MobileNetV2.mlmodel dosyasının 32 bit olanı projeye eklendi.(Ekleme olayı Xcode projesi açıkken dizine sürükle ve bırak şeklinde)
    Sonra Classifying Images with Vision and Core ML code view sayfasına geçildi.(Kod incelemesi ve nasıl kullanacağımızın bilgisi için)

---
+++ MachineLearningImageRecognition/MachineLearningImageRecognition/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognition
+//
+//  Created by Adem Deliaslan on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
