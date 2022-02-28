9bd0ca8310f877540952f743ec73995c499116f1
Adem Deliaslan <deliaslan@gmail.com>
2022-02-28 22:48:37 +0300

    AngryBirdesClone çarpışma sonrası skoru yükseltme.
    Çok sağlıklı olmasada her temasa bir puan artırma gerçekleşmektedir.

--- AngryBirdClone/AngryBirdClone/GameScene.swift
+++ AngryBirdClone/AngryBirdClone/GameScene.swift
@@ -8,13 +8,23 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{ //SKPhysicsContactDelegate dünyamızın algılaması için ekledik
     
     //    var bird2 = SKSpriteNode()
     var bird = SKSpriteNode()
     var box1 = SKSpriteNode(), box2 = SKSpriteNode(), box3 = SKSpriteNode(), box4 = SKSpriteNode(), box5 = SKSpriteNode()
     var gameStarted = false
     var originalPosition : CGPoint?
+    
+    var score = 0
+    var scoreLabel = SKLabelNode()
+    
+    enum Collidertype: UInt32 {
+        case Bird = 1
+        case Box = 2
+        case Ground = 4
+        case Tree = 8
+    }
     
     override func didMove(to view: SKView) {
         //        let texture = SKTexture(imageNamed: "bird")
@@ -28,6 +38,7 @@ class GameScene: SKScene {
         //Physics Body
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame) // ekran sınırlarını belirliyoruz.
         self.scene?.scaleMode = .aspectFit
+        self.physicsWorld.contactDelegate = self //kendi dünyamzı algılatmak için
         
         // Bird
         bird = childNode(withName: "bird") as! SKSpriteNode
@@ -39,6 +50,11 @@ class GameScene: SKScene {
         bird.physicsBody?.isDynamic = true
         bird.physicsBody?.mass = 0.20
         originalPosition = bird.position
+        
+        //çarpışmaları algılamak içim
+        bird.physicsBody?.contactTestBitMask = Collidertype.Bird.rawValue
+        bird.physicsBody?.categoryBitMask = Collidertype.Bird.rawValue
+        bird.physicsBody?.collisionBitMask = Collidertype.Box.rawValue
         
         //Boxes
         
@@ -52,12 +68,16 @@ class GameScene: SKScene {
         box1.physicsBody?.allowsRotation = true
         box1.physicsBody?.mass = 0.2
         
+        box1.physicsBody?.collisionBitMask = Collidertype.Bird.rawValue //çarpışmayı algılama
+        
         box2 = childNode(withName: "box2") as! SKSpriteNode
         box2.physicsBody = SKPhysicsBody(rectangleOf: size)
         box2.physicsBody?.isDynamic = true
         box2.physicsBody?.affectedByGravity = true
         box2.physicsBody?.allowsRotation = true
         box2.physicsBody?.mass = 0.2
+        
+        box2.physicsBody?.collisionBitMask = Collidertype.Bird.rawValue
         
         box3 = childNode(withName: "box3") as! SKSpriteNode
         box3.physicsBody = SKPhysicsBody(rectangleOf: size)
@@ -66,12 +86,16 @@ class GameScene: SKScene {
         box3.physicsBody?.allowsRotation = true
         box3.physicsBody?.mass = 0.2
         
+        box3.physicsBody?.collisionBitMask = Collidertype.Bird.rawValue
+        
         box4 = childNode(withName: "box4") as! SKSpriteNode
         box4.physicsBody = SKPhysicsBody(rectangleOf: size)
         box4.physicsBody?.isDynamic = true
         box4.physicsBody?.affectedByGravity = true
         box4.physicsBody?.allowsRotation = true
         box4.physicsBody?.mass = 0.2
+        
+        box4.physicsBody?.collisionBitMask = Collidertype.Bird.rawValue
         
         box5 = childNode(withName: "box5") as! SKSpriteNode
         box5.physicsBody = SKPhysicsBody(rectangleOf: size)
@@ -80,8 +104,30 @@ class GameScene: SKScene {
         box5.physicsBody?.allowsRotation = true
         box5.physicsBody?.mass = 0.2
         
+        box5.physicsBody?.collisionBitMask = Collidertype.Bird.rawValue
+        
+        //Label
+        scoreLabel.fontName = "Helvetica"
+        scoreLabel.color = .brown
+        scoreLabel.fontSize = 45
+        scoreLabel.text = "0"
+        
+        scoreLabel.position = CGPoint(x: 0, y: self.frame.height / 3)
+        scoreLabel.zPosition = 2
+        self.addChild(scoreLabel)
+        
+        
     }
-    
+    //kendi dünyamızdan birbirine değenler varmı
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        if contact.bodyA.collisionBitMask == Collidertype.Bird.rawValue || contact.bodyB.collisionBitMask == Collidertype.Bird.rawValue {
+//            print("Contacted") //test code
+            score += 1
+            scoreLabel.text = String(score)
+            
+        }
+    }
     
     func touchDown(atPoint pos : CGPoint) {
         
@@ -153,7 +199,7 @@ class GameScene: SKScene {
                                 bird.physicsBody?.applyImpulse(impulse)
                                 bird.physicsBody?.affectedByGravity = true
                                 
-                               // gameStarted = true
+                                gameStarted = true
                             }
                         }
                     }
@@ -169,5 +215,14 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if let birdPhysicsBody = bird.physicsBody {
+            if birdPhysicsBody.velocity.dx <= 0 && birdPhysicsBody.velocity.dy <= 0 && birdPhysicsBody.angularVelocity <= 0 && gameStarted == true {
+                bird.physicsBody?.affectedByGravity = false
+                birdPhysicsBody.velocity = CGVector(dx: 0, dy: 0)
+                bird.zPosition = 1
+                bird.position = originalPosition!
+                gameStarted = false
+            }
+        }
     }
 }
