6341f55ea0c51a578e6a548cef14cfd1ab0dfa70
Adem Deliaslan <deliaslan@gmail.com>
2022-03-03 21:59:07 +0300

    Tuple, If let ve Guard let kullanımını gördük.

--- AdvancedSwiftProject/AdvancedSwiftProject/main.swift
+++ AdvancedSwiftProject/AdvancedSwiftProject/main.swift
@@ -60,3 +60,64 @@ print("afterClass: \(classJames.age)")
 print("beforeStruct: \(structJames.age)")
 structJames.happyBirthday()
 print("afterStruct: \(structJames.age)")
+
+
+//TUPLE
+
+let myTuple = (1,3)
+print(myTuple.0)
+
+let myTuple2 = (1,3,5)
+print(myTuple2.2)
+
+var myTuple3 = (1,3,5)
+
+myTuple3.2 = 20
+print(myTuple3.2)
+
+let myTuple4 = ("Adem", 38)
+print(myTuple4.0)
+
+let myTuple5 = (10, [10,20,30])
+
+print(myTuple5.1[1])
+
+let preDefinedTuple: (String, String)
+
+preDefinedTuple.0 = "Adem"
+preDefinedTuple.1 = "Çiğdem"
+
+print(preDefinedTuple)
+
+let newTuple = (name: "Adem", metallica: true) //tuple lara isim atayabiliyoruz.
+
+print(newTuple.metallica)
+
+//GUARD LET VS IF LET
+
+//GUARD -> DAHA OLUMSUZ BŞR YAKLAŞIM && confident
+
+// IF -> DAHA POZİTİF
+
+
+let myNumber = "elma"
+//let myNumber = 5
+
+func convertToIntegerGuard (stringInput : String) -> Int {
+    guard let myInteger = Int(stringInput) else {
+        return 0
+    }
+    return myInteger
+}
+
+func convertToIntegerIf (stringInput : String) -> Int {
+    if let myInteger = Int(stringInput) {
+        return myInteger
+    } else {
+        return 0
+    }
+}
+
+
+print("If: \(convertToIntegerIf(stringInput: myNumber))")
+print("Guard: \(convertToIntegerGuard(stringInput: myNumber))")
