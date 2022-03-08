60a8200390d9623446aedba9c11dfa5b0b6f61d8
Adem Deliaslan <deliaslan@gmail.com>
2022-03-08 22:23:35 +0300

    SnapchatClone Upload view alanında resim seçme işlemi tamamlandı

--- 31- SnapChatClone/SnapChatClone/ViewControllers/UploadVC.swift
+++ 31- SnapChatClone/SnapChatClone/ViewControllers/UploadVC.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class UploadVC: UIViewController {
+class UploadVC: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var uploadImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        uploadImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(choosePhoto))
+        uploadImageView.addGestureRecognizer(gestureRecognizer)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func choosePhoto(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        uploadImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func uploadClicked(_ sender: Any) {
