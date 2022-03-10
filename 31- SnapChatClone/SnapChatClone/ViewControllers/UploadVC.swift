4ae404550cbf2a130bc035163c05cb4d79610b03
Adem Deliaslan <deliaslan@gmail.com>
2022-03-10 07:41:46 +0300

    SnapchatClone Upload ile görsellerin yüklenmesi sağlandı.
    FireStore Databse Rule kısmında sorun yaşayacak olursanız bu komutlar ile deneyiniz.,
    
    rules_version = '2';
    service cloud.firestore {
      match /databases/{database}/documents {
        match /{document=**} {
          allow read, write: if true;
    
        }
      }
    }

--- 31- SnapChatClone/SnapChatClone/ViewControllers/UploadVC.swift
+++ 31- SnapChatClone/SnapChatClone/ViewControllers/UploadVC.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import Firebase //eklendi
 
 class UploadVC: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var uploadImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         uploadImageView.isUserInteractionEnabled = true
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(choosePhoto))
         uploadImageView.addGestureRecognizer(gestureRecognizer)
@@ -32,8 +33,49 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate & UINavigation
     }
     
     @IBAction func uploadClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = uploadImageView.image?.jpegData(compressionQuality: 0.5) {
+            let uuid = UUID()
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            
+            imageReference.putData(data, metadata: nil) { metadata, error in
+                if error != nil {
+                    self.makeAlert(title: "OK", message: error?.localizedDescription ?? "Error")
+                } else {
+                    imageReference.downloadURL { url, error in
+                        if error == nil {
+                            
+                            let imageUrl = url?.absoluteString
+                            let fireStore = Firestore.firestore()
+                            
+                            let snapDictionary = ["imagUrl" : imageUrl!, "snapOwner": UserSingleton.sharedUserInfo.username, "date": FieldValue.serverTimestamp()] as [String : Any]
+                            
+                            
+                            fireStore.collection("Snaps").addDocument(data: snapDictionary) { error in
+                                if error != nil {
+                                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                                } else {
+                                    self.tabBarController?.selectedIndex = 0
+                                    self.uploadImageView.image = UIImage(named: "tapToSelect.png")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
- 
-
+    func makeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
