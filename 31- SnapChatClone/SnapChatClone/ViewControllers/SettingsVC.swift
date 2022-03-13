187930637deb6a64516827763fdbb8eb513eee77
Adem Deliaslan <deliaslan@gmail.com>
2022-03-13 08:44:53 +0300

    SnapchatClone SignOut butonunu kodla yazıp revize eettik.
    Farklı ekran türlerinde yaşanan sorun sebebiyle kodla yazma ihtiyacı doğdu.

--- 31- SnapChatClone/SnapChatClone/ViewControllers/SettingsVC.swift
+++ 31- SnapChatClone/SnapChatClone/ViewControllers/SettingsVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase //eklendi
+import MapKit
 
 class SettingsVC: UIViewController {
 
@@ -14,10 +15,26 @@ class SettingsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        let logOutButton = UIButton(frame: CGRect(x: 10, y: 10, width: self.view.frame.width * 0.3, height: self.view.frame.height * 0.08))
+        
+        logOutButton.center = self.view.center
+        logOutButton.backgroundColor = .gray
+        logOutButton.setTitle("Sign Out", for: .normal)
+        logOutButton.addTarget(self, action: #selector(logOutClickedButton2), for: .touchUpInside)
+
+        logOutButton.layer.cornerRadius = 10; // this value vary as per your desire
+        logOutButton.clipsToBounds = true
+        
+        self.view.addSubview(logOutButton)
+
+        
+        
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func logOutClicked(_ sender: Any) {
+    
+    @objc func logOutClickedButton2(){
         do {
             try Auth.auth().signOut()
             self.performSegue(withIdentifier: "toSignInVC", sender: nil)
@@ -25,6 +42,7 @@ class SettingsVC: UIViewController {
             makeAlert(title: "Error", message: "SignOut Error!")
         }
     }
+    
     func makeAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
