11311c175bd121c86e6a52e4f18dad861c03ce3d
Adem Deliaslan <deliaslan@gmail.com>
2022-03-20 12:00:18 +0300

    SwiftUI ile tasarlayacağımız projeyi oluşturduk

---
+++ CryptoCrazySwiftUI/CryptoCrazySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoCrazySwiftUI
+//
+//  Created by Adem Deliaslan on 20.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
