46aa0f55ef01aa05300b01549dbbb884a64884c7
Adem Deliaslan <deliaslan@gmail.com>
2022-03-29 12:07:14 +0300

    Composing Complex Interfaces - Add a Category View

---
+++ LandmarksSwiftUI/LandmarksSwiftUI/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  LandmarksSwiftUI
+//
+//  Created by Adem Deliaslan on 29.03.2022.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hellooooooooo")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
