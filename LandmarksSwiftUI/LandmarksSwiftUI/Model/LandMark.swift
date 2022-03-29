b26c5c633dd9f3df0448d4495af762f2e648ceed
Adem Deliaslan <deliaslan@gmail.com>
2022-03-29 12:38:20 +0300

    Complete the Category View

--- LandmarksSwiftUI/LandmarksSwiftUI/Model/LandMark.swift
+++ LandmarksSwiftUI/LandmarksSwiftUI/Model/LandMark.swift
@@ -16,6 +16,7 @@ struct LandMark: Hashable, Codable, Identifiable {
     var state: String
     var description: String
     var isFavorite: Bool
+    var isFeatured: Bool
     
     var category: Category
     enum Category: String, CaseIterable, Codable {
