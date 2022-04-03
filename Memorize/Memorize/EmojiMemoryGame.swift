cb92fd6077fc3d0cd2f4fad31ab628da53da359c
Adem Deliaslan <deliaslan@gmail.com>
2022-04-03 21:38:00 +0300

    Clean code

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -15,11 +15,11 @@ class EmojiMemoryGame {
     
     static func createMemoryGame() -> MemoryGame<String> {
         MemoryGame<String>(numberOfPairOfCards: 4) { pairIndex in
-            EmojiMemoryGame.emojis[pairIndex]
+            emojis[pairIndex]
         }
     }
     
-    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
