d4c2ca5f83df211457aa77fee87ea36d1e40bf6d
Adem Deliaslan <deliaslan@gmail.com>
2022-05-28 22:08:33 +0300

    Proje oluşturuldu.

---
+++ Firebase101/Firebase101/Firebase101App.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase101App.swift
+//  Firebase101
+//
+//  Created by Adem Deliaslan on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Firebase101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
